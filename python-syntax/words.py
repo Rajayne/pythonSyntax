14e9c71925cfec95af961e29bcb4565536d04b1d
Rajayne <ryann.j.nealey@gmail.com>
2023-02-01 11:50:38 -0800

    Created nested loop to check the starting letter of each word to match second arg in function

--- python-syntax/words.py
+++ python-syntax/words.py
@@ -1,9 +1,11 @@
-def print_upper_words(words, start_letter):
-    """ Function takes words from a list and prints them in uppercase """
+def print_upper_words(words, start_letters):
+    """ Function takes words from a list and prints them in uppercase 
+        Only prints words whose starting letter is contained in start_letters"""
     for word in words:
-        if word[0] is start_letter:
-            print(word.upper())
-        else:
-            continue        
+        for letter in start_letters:
+            if word[0] == letter:
+                print(word.upper())
+            else:
+                continue      
 
-print_upper_words(["ello", "hey", "goodbye", "yo", "yes"], "e")
+print_upper_words(["hello", "hey", "goodbye", "yo", "yes"], ["g","h"])
