f3621d2ee136dfaf9cc4baa2001dd26831856e36
Rajayne <ryann.j.nealey@gmail.com>
2023-02-01 11:34:25 -0800

    Add argument for start letter and check first letter of each word before printing

--- python-syntax/words.py
+++ python-syntax/words.py
@@ -1,5 +1,9 @@
-def print_upper_words(words):
+def print_upper_words(words, start_letter):
+    """ Function takes words from a list and prints them in uppercase """
     for word in words:
-        print(word.upper())
+        if word[0] is start_letter:
+            print(word.upper())
+        else:
+            continue        
 
-print_upper_words(["hello", "hey", "goodbye", "yo", "yes"])
+print_upper_words(["ello", "hey", "goodbye", "yo", "yes"], "e")
